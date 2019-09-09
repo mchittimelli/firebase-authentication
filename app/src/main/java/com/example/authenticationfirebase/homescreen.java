ca0c7a57eb539da90d20d92ab0ff19cab461c51a
temp <temp@MATRIXCOLLEGE.LCL>
2019-09-09 16:44:00 -0400

    added firebase and sync success

---
+++ app/src/main/java/com/example/authenticationfirebase/homescreen.java
@@ -0,0 +1,44 @@
+package com.example.authenticationfirebase;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class homescreen extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+    public homescreen() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_homescreen, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+
+
+
+}
---
+++ app/src/main/java/com/example/authenticationfirebase/register.java
@@ -0,0 +1,37 @@
+package com.example.authenticationfirebase;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class register extends Fragment {
+
+    public register() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+
+}
